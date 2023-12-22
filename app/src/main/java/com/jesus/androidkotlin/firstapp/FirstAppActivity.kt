9af651b3cfabdffa34d80f8da44af00923d047af
Jesus Pequeno <jesus.pequeno@lisdatasolutions.com>
2023-12-22 22:26:11 +0100

    Añadir campo de texto de nombre y str

--- app/src/main/java/com/jesus/androidkotlin/firstapp/FirstAppActivity.kt
+++ app/src/main/java/com/jesus/androidkotlin/firstapp/FirstAppActivity.kt
@@ -2,11 +2,24 @@ package com.jesus.androidkotlin.firstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatEditText
 import com.jesus.androidkotlin.R
 
 class FirstAppActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first_app)
+        val btnStart = findViewById<AppCompatButton>(R.id.btnStart)
+        val etName = findViewById<AppCompatEditText>(R.id.etName)
+
+        btnStart.setOnClickListener {
+            val name = etName.text.toString()
+
+            if (name.isNotEmpty()) {
+                Log.i("Hola", "texto boton $name")
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/jesus/androidkotlin/firstapp/ResultActivity.kt
@@ -0,0 +1,12 @@
+package com.jesus.androidkotlin.firstapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jesus.androidkotlin.R
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+    }
+}
