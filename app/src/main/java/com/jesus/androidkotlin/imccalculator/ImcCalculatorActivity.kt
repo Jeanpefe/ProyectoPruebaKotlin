3944cb9036825799a947a023db8df77ecbe0d7cf
Jesus Pequeno <jesus.pequeno@lisdatasolutions.com>
2023-12-23 13:17:13 +0100

    Nuevos ficheros calculadora IMC y cambio AndroidManifest

---
+++ app/src/main/java/com/jesus/androidkotlin/imccalculator/ImcCalculatorActivity.kt
@@ -0,0 +1,12 @@
+package com.jesus.androidkotlin.imccalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jesus.androidkotlin.R
+
+class ImcCalculatorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_imc_calculator)
+    }
+}
