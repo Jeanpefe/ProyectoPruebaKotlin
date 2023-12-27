4071563ca0221cb2123ed7cd84f023f107b31e54
Jesus Pequeno <jesus.pequeno@lisdatasolutions.com>
2023-12-27 18:56:48 +0100

    Boton TodoApp en el menu

--- app/src/main/java/com/jesus/androidkotlin/firstapp/MenuActivity.kt
+++ app/src/main/java/com/jesus/androidkotlin/firstapp/MenuActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.widget.Button
 import com.jesus.androidkotlin.R
 import com.jesus.androidkotlin.imccalculator.ImcCalculatorActivity
+import com.jesus.androidkotlin.todoapp.TodoActivity
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,16 +14,22 @@ class MenuActivity : AppCompatActivity() {
         setContentView(R.layout.activity_menu)
         val btnSaludApp = findViewById<Button>(R.id.btnSaludApp)
         val btnImcApp = findViewById<Button>(R.id.btnIMCApp)
+        val btnTodoApp = findViewById<Button>(R.id.btnTodoApp)
         btnSaludApp.setOnClickListener {navigateToSaludApp()}
         btnImcApp.setOnClickListener {navigateToImcApp()}
+        btnTodoApp.setOnClickListener {navigateToTodoApp()}
     }
 
-    fun navigateToSaludApp(){
+    private fun navigateToSaludApp(){
         val intent = Intent(this, FirstAppActivity::class.java)
         startActivity(intent)
     }
-    fun navigateToImcApp(){
+    private fun navigateToImcApp(){
         val intent = Intent(this, ImcCalculatorActivity::class.java)
+        startActivity(intent)
+    }
+    private fun navigateToTodoApp(){
+        val intent = Intent(this, TodoActivity::class.java)
         startActivity(intent)
     }
 }
--- readme.md
+++ readme.md
@@ -4,8 +4,8 @@
 Aplicación sencilla para navegar entre miniproyectos con Kotlin y AndroidStudio:
 - Mostrar información básica que se introduce como texto
 - Calculadora IMC
+- TODO App
 
 # TODO
 - [ ] Poder elegir modo claro y modo oscuro
-- [ ] Mostrar el IMC
 - [ ] Poner el botón de Calcular IMC y de Volver en la misma posición
