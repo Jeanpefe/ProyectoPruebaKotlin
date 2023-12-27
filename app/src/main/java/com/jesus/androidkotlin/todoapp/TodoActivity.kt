9be86967488a010a8dba84b521d538b7080f633e
Jesus Pequeno <jesus.pequeno@lisdatasolutions.com>
2023-12-27 23:44:03 +0100

    Primera versión del recycledView con datos de 3 categorias

--- app/src/main/java/com/jesus/androidkotlin/todoapp/TodoActivity.kt
+++ app/src/main/java/com/jesus/androidkotlin/todoapp/TodoActivity.kt
@@ -2,12 +2,18 @@ package com.jesus.androidkotlin.todoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.jesus.androidkotlin.R
 
 class TodoActivity : AppCompatActivity() {
-
+    private val categories = listOf(
+        TaskCategory.Business,
+        TaskCategory.Personal,
+        TaskCategory.Other
+    )
     private lateinit var rvCategories: RecyclerView
+    private lateinit var categoriesAdapter: CategoriesAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_todo)
@@ -20,6 +26,8 @@ class TodoActivity : AppCompatActivity() {
     }
 
     private fun initUI() {
-
+        categoriesAdapter = CategoriesAdapter(categories)
+        rvCategories.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        rvCategories.adapter = categoriesAdapter
     }
 }
