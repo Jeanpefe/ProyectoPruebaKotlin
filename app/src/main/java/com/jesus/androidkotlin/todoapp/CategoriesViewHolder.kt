8543f6d3a5245c487b4de0a0b70e1559f69e15a3
Jesus Pequeno <jesus.pequeno@lisdatasolutions.com>
2023-12-28 01:18:48 +0100

    recycledView con nombres y colores de categorias

--- app/src/main/java/com/jesus/androidkotlin/todoapp/CategoriesViewHolder.kt
+++ app/src/main/java/com/jesus/androidkotlin/todoapp/CategoriesViewHolder.kt
@@ -2,6 +2,7 @@ package com.jesus.androidkotlin.todoapp
 
 import android.view.View
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.jesus.androidkotlin.R
 
@@ -10,5 +11,26 @@ class CategoriesViewHolder(view:View) : RecyclerView.ViewHolder(view) {
     private val divider: View = view.findViewById(R.id.divider)
     fun render(TaskCategory: TaskCategory) {
         tvCategoryName.text = "EJEMPLO"
+
+        when (TaskCategory) {
+            com.jesus.androidkotlin.todoapp.TaskCategory.Business -> {
+                tvCategoryName.text = "Negocios"
+                divider.setBackgroundColor(
+                    ContextCompat.getColor(divider.context, R.color.todo_business_category)
+                )
+            }
+            com.jesus.androidkotlin.todoapp.TaskCategory.Other -> {
+                tvCategoryName.text = "Otros"
+                divider.setBackgroundColor(
+                    ContextCompat.getColor(divider.context, R.color.todo_other_category)
+                )
+            }
+            com.jesus.androidkotlin.todoapp.TaskCategory.Personal -> {
+                tvCategoryName.text = "Personal"
+                divider.setBackgroundColor(
+                    ContextCompat.getColor(divider.context, R.color.todo_personal_category)
+                )
+            }
+        }
     }
 }
